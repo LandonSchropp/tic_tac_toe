c732b2904facb4a30c6e67b2cbd6ba3baa861e70
Landon Schropp <schroppl@gmail.com>
2017-04-17 01:07:20 -0700

    Add build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 'use strict';
 
-const gulp = require('gulp');
 const del = require('del');
+const gulp = require('gulp');
+const runSequence = require('run-sequence');
 
 gulp.task('clean', () => {
   return del(["build"]);
@@ -9,4 +10,8 @@ gulp.task('clean', () => {
 
 gulp.task('copy', () => {
   return gulp.src("source/**/*.html").pipe(gulp.dest("build"));
+});
+
+gulp.task('build', (callback) => {
+  runSequence('clean', 'copy', callback);
 });
