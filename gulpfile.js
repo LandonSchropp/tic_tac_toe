7a7bf5fcbad57ebdae2293c1c1234c69423b097b
Landon Schropp <schroppl@gmail.com>
2017-04-17 23:03:59 -0700

    Add a watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const connect = require('gulp-connect');
 const del = require('del');
 const gulp = require('gulp');
 const rollup = require('rollup-stream');
@@ -7,6 +8,7 @@ const runSequence = require('run-sequence');
 const sass = require('gulp-sass');
 const sassGlob = require('gulp-sass-glob');
 const source = require('vinyl-source-stream');
+const watch = require('gulp-watch');
 
 const rollupConfig = require('./rollup.config');
 
@@ -14,15 +16,15 @@ gulp.task('clean', () => {
   return del(["build"]);
 });
 
-gulp.task('copy', () => {
+gulp.task('html', () => {
   return gulp.src("source/**/*.html").pipe(gulp.dest("build"));
 });
 
 gulp.task('build', (callback) => {
-  runSequence('clean', 'copy', callback);
+  runSequence('clean', 'html', 'stylesheets', 'javascripts', callback);
 });
 
-gulp.task('stylesheets', function () {
+gulp.task('stylesheets', () => {
   return gulp
     .src('source/stylesheets/index.sass')
     .pipe(sassGlob())
@@ -35,3 +37,21 @@ gulp.task('javascripts', () => {
     .pipe(source('index.js'))
     .pipe(gulp.dest('build/javascripts'));
 });
+
+gulp.task('watch', ["build"], () => {
+
+  // Create a callback that runs a task.
+  function run(task) {
+    return () => runSequence(task);
+  }
+
+  // Kick off the watchers
+  watch("source/images/**", run('images'));
+  watch("source/**/*.html", run('html'));
+  watch("source/javascripts/**/*.js", run('javascripts'));
+  watch("source/stylesheets/**/*.sass", run('stylesheets'));
+
+  // Start the server
+  connect.server({ root: "build", livereload: true });
+});
+
