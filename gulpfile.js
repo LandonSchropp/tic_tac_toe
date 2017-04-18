c3e633a8b4568c75e93afab12d05fce406037725
Landon Schropp <schroppl@gmail.com>
2017-04-17 22:44:50 -0700

    Add the stylesheets

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const del = require('del');
 const gulp = require('gulp');
 const rollup = require('rollup-stream');
 const runSequence = require('run-sequence');
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
 const source = require('vinyl-source-stream');
 
 const rollupConfig = require('./rollup.config');
@@ -18,6 +20,14 @@ gulp.task('copy', () => {
 
 gulp.task('build', (callback) => {
   runSequence('clean', 'copy', callback);
+});
+
+gulp.task('stylesheets', function () {
+  return gulp
+    .src('source/stylesheets/index.sass')
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(gulp.dest('build/stylesheets'));
 });
 
 gulp.task('javascripts', () => {
