e8b85e67f6474d54fab421291ddfd735bdb560f2
Landon Schropp <schroppl@gmail.com>
2017-04-17 23:44:34 -0700

    Fix issues with JavaScript task
    Rollup needs a couple plugins in order to properly compile JavaScript
    for the web.

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,15 @@
+"use strict";
+
+const commonjs = require('rollup-plugin-commonjs');
+const resolve = require('rollup-plugin-node-resolve');
+
 module.exports = {
   entry: 'source/javascripts/index.js',
-  format: 'cjs'
+  format: 'iife',
+  plugins: [
+    resolve(),
+    commonjs({
+      include: 'node_modules/**'
+    })
+  ]
 };
