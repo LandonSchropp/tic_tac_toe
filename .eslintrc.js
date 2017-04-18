144e8af66f903dc785c8404243ba69d7f2512c40
Landon Schropp <schroppl@gmail.com>
2017-04-17 22:32:52 -0700

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      impliedStrict: true,
+      experimentalObjectRestSpread: true
+    }
+  },
+  env: {
+    browser: true,
+    mocha: true,
+    node: true,
+    es6: true
+  }
+};
