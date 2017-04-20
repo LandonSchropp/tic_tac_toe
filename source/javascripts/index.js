5c53f5be7e8c1e15ab58b7c4474d8ad840148826
Landon Schropp <schroppl@gmail.com>
2017-04-19 22:11:30 -0700

    Add the board to the game

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,1 +1,33 @@
-console.log("Hello!");
+import _ from "lodash";
+
+let game = new Phaser.Game(380, 720, Phaser.AUTO, '', { preload, create, update });
+
+function preload() {
+
+  // Load the images
+  let images = {
+    board3: "board3.png",
+    board4: "board4.png",
+    board5: "board5.png",
+    o: "o.png",
+    x: "x.png"
+  };
+
+  _.forEach(images, (path, key) => game.load.image(key, `/images/${ path }`));
+}
+
+function create() {
+
+  // Add the board
+  let board = game.add.sprite(0, 0, 'board3');
+  board.anchor.setTo(0.5, 0.5);
+  board.position = game.world.bounds.size().multiply(0.5, 0.5);
+
+  // Scale the board so it properly fits in the canvas
+  let scale = game.world.bounds.width / board.width * 0.9;
+  board.scale.setTo(scale, scale);
+}
+
+function update() {
+
+}
