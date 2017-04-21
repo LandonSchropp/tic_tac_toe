db4c547661b1d034b54599d49288524ae83f26ef
Landon Schropp <schroppl@gmail.com>
2017-04-20 22:22:36 -0700

    Detect board click positions

--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,6 +1,6 @@
 import _ from "lodash";
 
-let game = new Phaser.Game(380, 720, Phaser.AUTO, '', { preload, create, update });
+let game = new Phaser.Game(380, 720, Phaser.AUTO, '', { preload, create });
 
 function preload() {
 
@@ -19,15 +19,22 @@ function preload() {
 function create() {
 
   // Add the board
-  let board = game.add.sprite(0, 0, 'board3');
-  board.anchor.setTo(0.5, 0.5);
-  board.position = game.world.bounds.size().multiply(0.5, 0.5);
+  let boardSprite = game.add.sprite(0, 0, 'board3');
+  boardSprite.anchor.setTo(0.5, 0.5);
+  boardSprite.position = game.world.bounds.size().multiply(0.5, 0.5);
 
   // Scale the board so it properly fits in the canvas
-  let scale = game.world.bounds.width / board.width * 0.9;
-  board.scale.setTo(scale, scale);
+  let scale = game.world.bounds.width / boardSprite.width * 0.9;
+  boardSprite.scale.setTo(scale, scale);
+
+  // Listen to clicks on the sprite
+  boardSprite.inputEnabled = true;
+  boardSprite.events.onInputDown.add(onBoardClick, this);
 }
 
-function update() {
+function onBoardClick(sprite, pointer) {
+  let { x, y, width, height } = sprite.getBounds();
 
+  let row = Math.floor((pointer.y - y) * 3 / height);
+  let column = Math.floor((pointer.x - x) * 3 / width);
 }
