ddd135da679de284e37e01e389128c8dec973c6d
Landon Schropp <schroppl@gmail.com>
2017-05-04 00:51:11 -0700

    Fix a couple small color issues

--- source/javascripts/palettes.js
+++ source/javascripts/palettes.js
@@ -3,7 +3,7 @@ import _ from "lodash";
 import colors from "../../temp/colors.json";
 
 let index = -1;
-let shuffledColors = _.concat(colors[0], _.identity(_.tail(colors)));
+let shuffledColors = _.concat(colors[0], _.shuffle(_.tail(colors)));
 
 export default {
 
