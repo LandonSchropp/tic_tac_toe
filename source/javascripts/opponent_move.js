2d03bc3f1c6df83b15185058a5789a6cf9e0ea7d
Landon Schropp <schroppl@gmail.com>
2017-05-03 17:53:33 -0700

    Let the player win every once in a while

--- source/javascripts/opponent_move.js
+++ source/javascripts/opponent_move.js
@@ -4,10 +4,19 @@ import { boardEmptySpaces, boardSet } from "./board";
 import minimaxScore from "./minimax_score";
 import { PLAYER_MARK, OPPONENT_MARK } from "./marks";
 
+// Defines how perfect the AI plays the game
+const DIFFICULTY = 0.8;
+
 // Returns a promise that resolves to the move for the opponent.
 export default function opponentMove(board) {
   return new Promise(resolve => {
-    resolve(_.minBy(boardEmptySpaces(board), (space) => {
+    let spaces = boardEmptySpaces(board);
+
+    // Every once in a while, play a random move
+    if (Math.random() > DIFFICULTY) { resolve(_.sample(spaces)); }
+
+    // Play the best possible move
+    resolve(_.minBy(spaces, (space) => {
       return minimaxScore(boardSet(board, ...space, OPPONENT_MARK), PLAYER_MARK)
     }));
   });
