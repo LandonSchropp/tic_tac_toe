7c4de325888167512b273ecbe5dbc116cf9552e0
Landon Schropp <schroppl@gmail.com>
2017-05-03 23:52:09 -0700

    Optimize minimaxScore
    I optimized the `minimaxScore` function by adding alpha-beta pruning.

--- source/javascripts/minimax_score.js
+++ source/javascripts/minimax_score.js
@@ -1,15 +1,19 @@
-import _ from "lodash";
 import { BOARD_SIZE, boardEmptySpaces, boardSet, boardWinner, boardNumberOfMarks } from "./board";
 import otherMark from "./other_mark";
 
-// This algorithm implemented by following the methodology in this excellent article:
-// https://mostafa-samir.github.io/Tic-Tac-Toe-AI/.
+// This algorithm implemented by following the methodology in [this](https://goo.gl/ktYe7r)
+// excellent article. The alpha-beta pruning component was taken from [here](https://goo.gl/Z4arB6).
 
 // A number larger than the total possible number of moves on the board.
 const SCORE_BOUND = BOARD_SIZE * BOARD_SIZE + 1;
 
+// Initialize the constants for alpha-beta pruning. Alpha represents the best option explored along
+// the path for `"x"`. Beta represents the best option explored for `"o"`.
+const ALPHA = Number.MIN_VALUE;
+const BETA = Number.MAX_VALUE;
+
 // Returns the minimax score for the provided board.
-export default function minimaxScore(board, mark) {
+export default function minimaxScore(board, mark, alpha = ALPHA, beta = BETA) {
 
   // If the game is over, return the appropriate score
   switch(boardWinner(board)) {
@@ -21,11 +25,29 @@ export default function minimaxScore(board, mark) {
       return 0;
   }
 
-  // Map all of the possible moves to their scores
-  let scores = boardEmptySpaces(board).map(([ row, column ]) => {
-    return minimaxScore(boardSet(board, row, column, mark), otherMark(mark));
-  });
+  let emptySpaces = boardEmptySpaces(board);
 
-  // Determine whether to maximize or minimize the score (depending on who's playing).
-  return (mark === "x" ? _.max : _.min)(scores);
+  // NOTE: The for loop is used as an optimiztion to let us break early.
+  for (let i = 0; i < emptySpaces.length; i++) {
+
+    // Get the score for the possible board
+    let score = minimaxScore(
+      boardSet(board, ...emptySpaces[i], mark),
+      otherMark(mark),
+      alpha,
+      beta
+    );
+
+    // Set alpha or beta.
+    if (mark === "x")
+      alpha = alpha > score ? alpha : score;
+    else
+      beta = beta < score ? beta : score;
+
+    // If the alpha-beta pruning condition is satisfied, break early.
+    if (alpha >= beta) break;
+  }
+
+  // Return the score
+  return mark === "x" ? alpha : beta;
 }
