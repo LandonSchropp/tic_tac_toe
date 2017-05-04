b7ef9f14d7573020bac8865aa1c35262f2e6f1d4
Landon Schropp <schroppl@gmail.com>
2017-05-03 23:59:04 -0700

    Bump down the difficulty of the opponent a smidge

--- source/javascripts/opponent_move.js
+++ source/javascripts/opponent_move.js
@@ -4,19 +4,19 @@ import { boardEmptySpaces, boardSet } from "./board";
 import minimaxScore from "./minimax_score";
 
 // Defines how perfect the AI plays the game
-const DIFFICULTY = 0.9;
+const DIFFICULTY = 0.85;
 
 // Returns a promise that resolves to the move for the opponent.
 export default function opponentMove(board) {
-  return new Promise(resolve => {
-    let spaces = boardEmptySpaces(board);
 
-    // Every once in a while, play a random move
-    if (Math.random() > DIFFICULTY) { resolve(_.sample(spaces)); }
+  // Get the available spaces
+  let emptySpaces = boardEmptySpaces(board);
 
-    // Play the best possible move
-    resolve(_.minBy(spaces, (space) => {
-      return minimaxScore(boardSet(board, ...space, "o"), "x")
-    }));
-  });
+  // Every once in a while, play a random move
+  if (Math.random() > DIFFICULTY) { return Promise.resolve(_.sample(emptySpaces)); }
+
+  // Play the best possible move
+  return Promise.resolve(_.minBy(emptySpaces, (space) => {
+    return minimaxScore(boardSet(board, ...space, "o"), "x")
+  }));
 }
